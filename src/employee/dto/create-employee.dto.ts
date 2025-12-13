9ce04eb753a9d2521a3c7fa3805313d72d69236a
Vidit-GKMIT <vidit@gkmit.co>
2025-12-13 20:49:26 +0530

    fix: add entities and to for employee and department (#3)
    * feat: create employee and department entities
    * feat: create employee and department dto
    * chore: minor update in code
    * chore: prettier formatting

--- src/employee/dto/create-employee.dto.ts
+++ src/employee/dto/create-employee.dto.ts
@@ -1,1 +1,33 @@
-export class CreateEmployeeDto {}
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  Length,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateEmployeeDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 50)
+  name: string;
+
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  salary: number;
+
+  @IsEmail()
+  @MaxLength(254)
+  @IsNotEmpty()
+  email: string;
+
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  departmentId: number;
+}
--- src/employee/entities/employee.entity.ts
+++ src/employee/entities/employee.entity.ts
@@ -1,1 +1,36 @@
-export class Employee {}
+import {
+  Check,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Department } from '../../department/entities/department.entity';
+
+@Entity('employees')
+@Check(`"salary" >= 0`)
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+  })
+  name: string;
+
+  @Column()
+  salary: number;
+
+  @Column({
+    length: 254,
+  })
+  email: string;
+
+  @ManyToOne(() => Department, (department) => department.employees, {
+    onDelete: 'RESTRICT',
+  })
+  @JoinColumn({ name: 'department_id' })
+  department: Department;
+}
