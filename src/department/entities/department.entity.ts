db9da74f264400bea953e0387d03d14c66dd80b4
Vidit Khandelwal <vidit@gkmit.co>
2025-12-13 15:07:04 +0530

    feat: create employee and department entities

--- src/department/entities/department.entity.ts
+++ src/department/entities/department.entity.ts
@@ -1,1 +1,48 @@
-export class Department {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  Timestamp,
+  OneToMany,
+} from 'typeorm';
+import { Employee } from '../../employee/entities/employee.entity';
+
+@Entity('departments')
+export class Department {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 25,
+    unique: true,
+  })
+  name: string;
+
+  @OneToMany(() => Employee, (employee) => employee.department)
+  employees: Employee[];
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    name: 'created_at',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Timestamp;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    name: 'updated_at',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamptz',
+    name: 'deleted_at',
+    default: null,
+    nullable: true,
+  })
+  deletedAt: Date;
+}
