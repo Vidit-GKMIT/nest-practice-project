a37c2b45c6ffc4f2a7161fc0a7522fb2f0daca5c
Vidit Khandelwal <vidit@gkmit.co>
2025-12-15 11:43:55 +0530

    fix: department controller

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,24 +1,22 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { DepartmentService } from './department.service';
+import { PaginationDto } from '../common/dto/pagnation.dto';
+import { IdDto } from '../common/dto/entityId.dto';
 
 @Controller('departments')
 export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
 
   @Get()
-  async findAll(
-    @Query('page') page: number = 1,
-    @Query('limit') limit: number = 10,
-  ) {
-    return this.departmentService.findAll(+page, +limit);
+  async findAll(@Query() paginationDto: PaginationDto) {
+    return this.departmentService.findAll(paginationDto);
   }
 
   @Get(':id/employees')
   async getAllEmployees(
-    @Param('id') id: number,
-    @Query('page') page: number = 1,
-    @Query('limit') limit: number = 10,
+    @Param() idDto: IdDto,
+    @Query() paginationDto: PaginationDto,
   ) {
-    return this.departmentService.getAllEmployees(+id, +page, +limit);
+    return this.departmentService.getAllEmployees(idDto, paginationDto);
   }
 }
--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -11,6 +11,8 @@ import {
 import { EmployeeService } from './employee.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { PaginationDto } from '../common/dto/pagnation.dto';
+import { IdDto } from '../common/dto/entityId.dto';
 
 @Controller('employees')
 export class EmployeeController {
@@ -22,25 +24,22 @@ export class EmployeeController {
   }
 
   @Get()
-  findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
-    return this.employeeService.findAll(+page, +limit);
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.employeeService.findAll(paginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.employeeService.findOne(+id);
+  findOne(@Param() idDto: IdDto) {
+    return this.employeeService.findOne(idDto);
   }
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateEmployeeDto: UpdateEmployeeDto,
-  ) {
-    return this.employeeService.update(+id, updateEmployeeDto);
+  update(@Param() idDto: IdDto, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+    return this.employeeService.update(idDto, updateEmployeeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.employeeService.remove(+id);
+  remove(@Param() idDto: IdDto) {
+    return this.employeeService.remove(idDto);
   }
 }
