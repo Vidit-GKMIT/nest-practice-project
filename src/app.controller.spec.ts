03495d5e6fcdf431f95bb7794e68b2e945df0ae8
Vidit Khandelwal <vidit@gkmit.co>
2025-12-15 11:44:42 +0530

    fix: minor changes

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,14 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return backend status response', () => {
+      const result = appController.getHello();
+
+      expect(result).toEqual({
+        message: 'Backend is running...!!',
+        success: true,
+        date: new Date(),
+      });
     });
   });
 });
